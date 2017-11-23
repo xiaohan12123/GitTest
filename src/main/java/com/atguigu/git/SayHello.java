8e218dc4ea67d0789cd402e0d0bd20d416de1a35
xiaohan <402795620@qq.com>
2017-11-23 10:48:29 +0800

    git测试

---
+++ src/main/java/com/atguigu/git/SayHello.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SayHello {
+	public static void main(String[] args) {
+		System.out.println("你好我的朋友");
+		List<String> list = new ArrayList<>();
+		list.add("你好");
+		System.out.println(list);
+	}
+}
